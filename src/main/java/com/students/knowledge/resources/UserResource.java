9e33a15f12758967955c59a2ce1ba10c5ce846c1
Handrei.M.Souza <handrei.m.souza@hotmail.com>
2023-03-19 16:10:13 -0300

    User method - insert

--- src/main/java/com/students/knowledge/resources/UserResource.java
+++ src/main/java/com/students/knowledge/resources/UserResource.java
@@ -5,11 +5,10 @@ import com.students.knowledge.domain.dto.UserDTO;
 import com.students.knowledge.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -30,5 +29,15 @@ public class UserResource {
     public ResponseEntity<UserDTO> findById (@PathVariable String id) {
         User user = _userService.findById(id);
         return ResponseEntity.ok().body(new UserDTO(user));
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> insert (@RequestBody User user) {
+        User userSaved = _userService.insert(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(userSaved.getId())
+                .toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
