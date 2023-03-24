f7a20122841191a95596585846aa050ab55d7dc5
Handrei Morais <handrei.m.souza@hotmail.com>
2023-03-23 23:27:07 -0300

    Inclusão da entidade Post
    Post Class
    Post Repository

--- src/main/java/com/students/knowledge/configs/Instantiation.java
+++ src/main/java/com/students/knowledge/configs/Instantiation.java
@@ -1,12 +1,16 @@
 package com.students.knowledge.configs;
 
+import com.students.knowledge.domain.Post;
 import com.students.knowledge.domain.User;
+import com.students.knowledge.repositories.PostRepository;
 import com.students.knowledge.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
@@ -14,16 +18,28 @@ public class Instantiation implements CommandLineRunner {
     @Autowired
     private UserRepository _userRepository;
 
+    @Autowired
+    private PostRepository _postRepository;
+
     @Override
     public void run(String... args) throws Exception {
 
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
         _userRepository.deleteAll();
+        _postRepository.deleteAll();
+
 
         User maria = new User(null, "Maria Brown", "maria@gmail.com", "1234");
         User alex = new User(null, "Alex Green", "alex@gmail.com", "1234");
         User bob = new User(null, "Bob Grey", "bob@gmail.com", "1234");
 
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
+        Post post2 = new Post(null, sdf.parse("23/03/2018"),  "Bom dia", "Acordei feliz hoje!", maria);
+
         _userRepository.saveAll(Arrays.asList(maria, alex, bob));
+        _postRepository.saveAll(Arrays.asList(post1, post2));
 
     }
 }
---
+++ src/main/java/com/students/knowledge/repositories/PostRepository.java
@@ -0,0 +1,9 @@
+package com.students.knowledge.repositories;
+
+import com.students.knowledge.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
